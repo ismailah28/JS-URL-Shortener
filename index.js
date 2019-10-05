247dec1c96e4707a4e2f5e0e59f0131137be5565
Ismaila Hassan <ismailah28@gmail.com>
2019-10-05 16:46:42 +0100

    Implement route for getting or creating short url

--- index.js
+++ index.js
@@ -11,6 +11,10 @@ connectDB();
 // Let app accept json data into our api
 app.use(express.json({ extended: false }));
 
+// Define routes
+app.use('/', require('./routes/index'));
+app.use('/api/url', require('./routes/url'));
+
 const PORT = 5000;
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
